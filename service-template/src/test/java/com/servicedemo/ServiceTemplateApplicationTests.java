8f567f0b44eb15e9b17662efe3280f170b8f7020
mknauff2 <mknauff2@flash.net>
2021-08-29 14:19:11 -0500

    initial commit

---
+++ service-template/src/test/java/com/servicedemo/ServiceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
