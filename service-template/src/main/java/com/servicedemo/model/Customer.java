dcd49bf2f6e4f8204ecb02fad89d333dbff1513b
mknauff2 <mknauff2@flash.net>
2023-05-06 10:16:20 -0500

    upgraded to spring boot 3.x

--- service-template/src/main/java/com/servicedemo/model/Customer.java
+++ service-template/src/main/java/com/servicedemo/model/Customer.java
@@ -3,14 +3,15 @@
  */
 package com.servicedemo.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
 import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 
 /**
  * @author mikeknauff
